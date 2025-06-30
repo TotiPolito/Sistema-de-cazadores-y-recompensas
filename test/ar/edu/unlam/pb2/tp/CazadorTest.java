2c21698df4c388ed7fe9331c1e0f14974274de4e
TotiCapo <totipolito@gmail.com>
2025-06-30 00:33:07 -0300

    Primer Commit
    TestPush

---
+++ test/ar/edu/unlam/pb2/tp/CazadorTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2.tp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CazadorTest {
+
+	@Test
+	public void quePuedoCrearUnCazador() {
+		assertTrue(true);;
+	}
+
+}
